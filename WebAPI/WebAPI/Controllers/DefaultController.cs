c4ca9dd88a2fe858b155a029745c94bf21c0baaf
weerut panjawattana <weerut.pan@gmail.com>
2017-05-09 14:03:07 +0700

    first commit

---
+++ WebAPI/WebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Hi(string name = "no name")
+        {
+            return Ok($"Hi {name}");
+        }
+
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Bye(string name = "no name")
+        {
+            return Ok($"Bye {name}");
+        }
+    }
+}
